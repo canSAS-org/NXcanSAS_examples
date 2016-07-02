7ce1c74b988a73943bd030da0cba10c6c052c478
Pete R Jemian <prjemian@gmail.com>
2016-07-02 12:38:45 -0400

    refactor to OOP

--- 1d_standard/python/xml_2_hdf5.py
+++ 1d_standard/python/xml_2_hdf5.py
@@ -15,34 +15,37 @@ import lxml.etree
 #from spec2nexus import eznx
 
 
-NS = dict(
-    cs1_0 = 'cansas1d/1.0',
-    cs1_1 = 'urn:cansas1d:1.1',
-    xsi = 'http://www.w3.org/2001/XMLSchema-instance',
-)
-
-
-def openXML(xmlFile):
+class canSAS1D_to_NXcanSAS(object):
     '''
     '''
-    if not os.path.exists(xmlFile):
-        raise IOError('file not found: ' + xmlFile)
     
-    doc = lxml.etree.parse(xmlFile)
-    root = doc.getroot()
-    # set the default XML namespace as 'cs'
-    NS['cs'] = {'1.0': NS['cs1_0'], '1.1': NS['cs1_1']}[root.attrib['version']]
-    return root
-
-
-def convert(xmlFile, hdf5File):
-    '''
-    '''
-    print xmlFile, hdf5File
-    src = openXML(xmlFile)
-    for sasentry in src.find('cs:SASentry'):
-        node_name = sasentry.attrib.get('name', 'sasentry')
-        print node_name
+    def __init__(self):
+        self.ns = dict(
+            cs1_0 = 'cansas1d/1.0',
+            cs1_1 = 'urn:cansas1d:1.1',
+            xsi = 'http://www.w3.org/2001/XMLSchema-instance',
+        )
+        self.xmlFile = None
+        self.hdf5File = None
+        self.root = None
+    
+    def open(self, xmlFile):
+        '''
+        '''
+        if not os.path.exists(xmlFile):
+            raise IOError('file not found: ' + xmlFile)
+        self.xmlFile = xmlFile
+        self.root = lxml.etree.parse(xmlFile).getroot()
+    
+    def write(self, hdf5File):
+        '''
+        '''
+        for node in self.root:
+            if node.tag.endswith('}SASentry'):
+                node_name = node.attrib.get('name', 'sasentry')
+                print node_name
+#     nx_root = eznx.makeFile(hdf5File)
+#     eznx.addAttributes(nx_root, creator='canSAS1d_to_NXcanSAS.py')
 
 
 def developer():
@@ -52,9 +55,10 @@ def developer():
     '''.strip().split()
     for fname in filelist:
         xmlFile = os.path.join('..', 'xml', 'bimodal-test1.xml')
-        # demo(xmlFile)
+        converter = canSAS1D_to_NXcanSAS()
+        converter.open(xmlFile)
         hdf5File = os.path.join('..', os.path.splitext(fname)[0] + '.h5')
-        convert(xmlFile, hdf5File)
+        converter.write(hdf5File)
 
 
 if __name__ == "__main__":
