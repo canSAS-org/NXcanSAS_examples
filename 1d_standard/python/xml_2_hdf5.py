722c5a6e532ba0b6a7f87ee8244d9c222df369c6
Pete R Jemian <prjemian@gmail.com>
2016-07-07 15:27:56 -0400

    basic start on the converter

--- 1d_standard/python/xml_2_hdf5.py
+++ 1d_standard/python/xml_2_hdf5.py
@@ -12,7 +12,16 @@ convert canSAS1d XML file to NXcanSAS HDF5 file
 import os
 import sys
 import lxml.etree
-#from spec2nexus import eznx
+from spec2nexus import eznx
+
+
+def safename(text):
+    '''
+    make sure text conforms to NeXus relaxed validItemName
+    
+    reg exp:  [A-Za-z_][\w_]*
+    '''
+    return text.replace(' ', '_')  # TODO: generalize
 
 
 class canSAS1D_to_NXcanSAS(object):
@@ -36,29 +45,53 @@ class canSAS1D_to_NXcanSAS(object):
             raise IOError('file not found: ' + xmlFile)
         self.xmlFile = xmlFile
         self.root = lxml.etree.parse(xmlFile).getroot()
+        self.ns['cs'] = {'1.0': self.ns['cs1_0'],
+                         '1.1': self.ns['cs1_1']}[self.root.attrib['version']]
     
     def write(self, hdf5File):
         '''
+        write the HDF5 file while parsing the XML file
         '''
-        for node in self.root:
-            if node.tag.endswith('}SASentry'):
-                node_name = node.attrib.get('name', 'sasentry')
-                print node_name
-#     nx_root = eznx.makeFile(hdf5File)
-#     eznx.addAttributes(nx_root, creator='canSAS1d_to_NXcanSAS.py')
+        nx_root = eznx.makeFile(hdf5File)
+        eznx.addAttributes(nx_root, creator='canSAS1d_to_NXcanSAS.py')
+        print self.xmlFile
+        x_sasentries = self.root.findall('cs:SASentry', self.ns)
+        default = None
+        for i, x_sasentry in enumerate(x_sasentries):
+            nm = safename(x_sasentry.attrib.get('name', 'sasentry_'+str(i)))
+            print nm
+            if default is None:
+                default = nm
+                eznx.addAttributes(nx_root, default=nm)
+            nx_sasdata = eznx.makeGroup(nx_root, 
+                                        nm, 'NXentry',
+                                        canSAS_class='SASentry')
+        nx_root.close()
+
+#         for node in self.root:
+#             if node.tag.endswith('}SASentry'):
+#                 node_name = node.attrib.get('name', 'x_sasentry')
+#                 print node_name
+
+    def p_sasentry(self):
+        '''
+        '''
 
 
 def developer():
     filelist = '''
     bimodal-test1.xml
     s81-polyurea.xml
+    1998spheres.xml
     '''.strip().split()
+    # filelist = os.listdir(os.path.join('..', 'xml'))
     for fname in filelist:
-        xmlFile = os.path.join('..', 'xml', 'bimodal-test1.xml')
-        converter = canSAS1D_to_NXcanSAS()
-        converter.open(xmlFile)
-        hdf5File = os.path.join('..', os.path.splitext(fname)[0] + '.h5')
-        converter.write(hdf5File)
+        if fname.endswith('.xml'):
+            xmlFile = os.path.join('..', 'xml', fname)
+            converter = canSAS1D_to_NXcanSAS()
+            converter.open(xmlFile)
+            hdf5File = os.path.join('..', os.path.splitext(fname)[0] + '.h5')
+            converter.write(hdf5File)
 
 
 if __name__ == "__main__":
